51949380e9e7cba571c7651a9bcbed9e349e418a
raonifduarte <80927549+raonifduarte@users.noreply.github.com>
2021-06-16 12:57:18 -0300

    Add files via upload

---
+++ packing.py
@@ -0,0 +1,28 @@
+def soma_2(a, b):
+    return a + b
+
+
+def soma_3(a, b, c):
+    return a + b + c
+
+
+def soma_n(*numeros):
+    soma = 0
+    for n in numeros:
+        soma += n
+    return soma
+
+
+if __name__ == '__main__':
+    print(soma_2(2, 3))
+    print(soma_3(2, 4, 6))
+
+    # packing
+    print(soma_n(1))
+    print(soma_n(1, 1))
+    print(soma_n(1, 1, 1, 1, 1, 1, 1))
+
+    # unpacking
+    nums = (1, 2, 3)
+    print(type(nums))
+    print(soma_3(*nums))
